c358face7be7dcdc8c2e617f0fc2d68416bc8a2e
Ivcota <ivcotad@gmail.com>
2022-04-07 13:28:51 -0700

    note clutter cleaned

--- server/schema.ts
+++ server/schema.ts
@@ -1,24 +1,5 @@
-/*
-Welcome to the schema! The schema is the heart of Keystone.
-
-Here we define our 'lists', which will then be used both for the GraphQL
-API definition, our database tables, and our Admin UI layout.
-
-Some quick definitions to help out:
-A list: A definition of a collection of fields with a name. For the starter
-  we have `User`, `Post`, and `Tag` lists.
-A field: The individual bits of data on your list, each with its own type.
-  you can see some of the lists in what we use below.
-
-*/
-
-// Like the `config` function we use in keystone.ts, we use functions
-// for putting in our config so we get useful errors. With typescript,
-// we get these even before code runs.
 import { Lists } from ".keystone/types";
 import { list } from "@keystone-6/core";
-// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
-// for the full list of fields.
 import {
   checkbox,
   image,
@@ -37,7 +18,6 @@ const isAdmin = ({ session }: { session: Session }) => session.data.isAdmin;
 
 hasRights - checks if the session holder owns the entity 
 isUser - checks if admin and is user
-
 */
 
 const hasRights = ({ item, session }: { item: any; session: Session }) => {
@@ -55,6 +35,8 @@ const isUser = ({ item, session }: { item: any; session: Session }) => {
 
   return item.id === (session as Session).data.id ? true : false;
 };
+
+/* The Schema that the backend applicaiton will be working from. */
 
 export const lists: Lists = {
   User: list({
