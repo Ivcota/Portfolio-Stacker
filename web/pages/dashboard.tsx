4b1308e28cd6a0f430708556583e7edef3d3ea5d
Ivcota <ivcotad@gmail.com>
2022-04-14 13:32:00 -0700

    dashboard page

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,17 +1,28 @@
+import { Container, Image, Stack, Text, Title } from "@mantine/core";
 import { NextPage } from "next";
 import React from "react";
 import CheckAuth from "../components/CheckAuth";
 import { useUser } from "../hooks/authHooks";
+import { baseURL } from "./../utils/url";
 
 const Dashboard: NextPage = () => {
   const { user } = useUser();
 
   return (
     <CheckAuth>
-      <div>
-        <h1>Dashboard</h1>
-        <p> Hello {user?.firstName} </p>
-      </div>
+      <Container>
+        <Title mt="lg" align="center">
+          Portfolio Stacker
+        </Title>
+        <Stack mt={100} align="center">
+          <Image
+            width={180}
+            radius={100}
+            src={baseURL + user?.profilePicture?.url}
+          />
+          <Text size="lg"> {user?.username} </Text>
+        </Stack>
+      </Container>
     </CheckAuth>
   );
 };
--- web/utils/url.ts
+++ web/utils/url.ts
@@ -1,1 +1,2 @@
+export const baseURL = "http://localhost:3000";
 export const graphqlurl = "http://localhost:3000/api/graphql";
