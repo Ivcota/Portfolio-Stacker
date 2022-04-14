dbcf0b90a6817e8227aabae22364a6e661eb51a1
Ivcota <ivcotad@gmail.com>
2022-04-14 02:25:31 -0700

    lightmode init

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,12 +1,59 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "@mantine/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  const [value, setValue] = useLocalStorage<ColorScheme>({
+    key: "color-scheme",
+    defaultValue: "dark",
+  });
+
+  useEffect(() => {
+    setColorScheme(value);
+  }, []);
+
+  useEffect(() => {
+    setValue(colorScheme);
+  }, [colorScheme]);
+
   return (
-    <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{
+          colorScheme: colorScheme,
+          colors: {
+            dark: [
+              "#F9FAFB",
+              "#F3F6F8",
+              "#E3E8F0",
+              "#CED1E8",
+              "#ACA9D8",
+              "#817FC3",
+              "#5C5AA2",
+              "#45457D",
+              "#33355A",
+              "#282B45",
+            ],
+          },
+        }}
+        withGlobalStyles
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Stack,
   createStyles,
+  useMantineColorScheme,
 } from "@mantine/core";
 import type { NextPage } from "next";
 
@@ -14,17 +15,37 @@ const useStyles = createStyles((theme) => ({
     minHeight: "100vh",
     paddingBottom: theme.spacing.xl,
   },
+  button: {
+    background:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.dark[9],
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[9]
+        : theme.colors.dark[0],
+    width: "18rem",
+    ":hover": {
+      background:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[3]
+          : theme.colors.dark[6],
+    },
+  },
 }));
 
 const Home: NextPage = () => {
   const { classes } = useStyles();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
     <Container>
       <Stack className={classes.base} justify="center" align="center">
         <Title align="center">Portfolio Stacker</Title>
         <Text align="center">Quickly Get Your Work Seen.</Text>
-        <Button color="white">Get Started</Button>
+        <Button onClick={() => toggleColorScheme()} className={classes.button}>
+          Get Started
+        </Button>
       </Stack>
     </Container>
   );
