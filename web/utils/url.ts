627877d78ab1d980ba3b886fbae3718e4b791139
Iverson Diles <ivcotad@gmail.com>
2022-06-10 21:23:16 -0700

    Update url.ts

--- web/utils/url.ts
+++ web/utils/url.ts
@@ -1,6 +1,6 @@
 // export const baseURL = "http://localhost:3000";
 // export const graphqlurl = "http://localhost:3000/api/graphql";
 
-export const baseURL = "https://portfolio-stacker-backend.herokuapp.com";
+export const baseURL = "https://portfolio-stacker-backend.herokuapp.com/";
 export const graphqlurl =
   "https://portfolio-stacker-backend.herokuapp.com/api/graphql";
